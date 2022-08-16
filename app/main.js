ca46864fd59bb8669da7ad5ce85423ef6d429fa1
Connor Dennison ⠕ <92693892+DaInfLoop@users.noreply.github.com>
2022-08-16 22:56:18 +0000

    added the labeller workflow for prs

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar: false, 
+    autoHideMenuBar: true, 
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
